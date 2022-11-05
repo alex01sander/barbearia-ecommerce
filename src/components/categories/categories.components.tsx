9ff6096e94a66ec74a6f978000b28d0f17e433d0
Alex Sander <alexsander01@hotmail.com.br>
2022-11-05 18:52:55 -0300

    feat: add firebase Convert

--- src/components/categories/categories.components.tsx
+++ src/components/categories/categories.components.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
 import Category from '../../types/category.types'
 import { CategoriesContainer, CategoriesContent } from './categories.styles'
-import axios from 'axios'
-import env from '../../config/env.congif'
 import CategoryItem from '../category-item/category-item.components'
+import { getDocs, collection } from '@firebase/firestore'
+import { db } from '../../config/firebase.config'
+import { categoryConverter } from '../../converters/firebase-converters'
 
 const Catergories = () => {
   const [categories, setCategories] = useState<Category[]>([])
@@ -11,9 +12,14 @@ const Catergories = () => {
   console.log(categories)
   const fecthCategories = async () => {
     try {
-      const { data } = await axios.get(`${env.apiUrl}/api/category`)
-      console.log({ data })
-      setCategories(data)
+      const categoriesFromFirestore: Category[] = []
+
+      const querySnapshot = await getDocs(collection(db, 'categories').withConverter(categoryConverter))
+
+      querySnapshot.forEach((doc: any) => {
+        categoriesFromFirestore.push(doc.data())
+      })
+      setCategories(categoriesFromFirestore)
     } catch (error) {
       console.log(error)
     }
