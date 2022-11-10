fc75a266603a0aedc4f288837e8471041f20ed66
Alex Sander <alexsander01@hotmail.com.br>
2022-11-10 20:15:00 -0300

    feat: add button Cart Components

--- src/components/product-item/product-item.component.tsx
+++ src/components/product-item/product-item.component.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from 'react'
 import Product from '../../types/products.types'
+import CustomButton from '../cutom-butto/custom-button.components'
 import { ProductContainer, ProductImage, ProductInfo } from './product-item.styles'
+import { BsCartPlus } from 'react-icons/bs'
 
 interface ProductItemProps {
     product: Product
@@ -9,7 +11,9 @@ interface ProductItemProps {
 const ProductItem: FunctionComponent<ProductItemProps> = ({ product }) => (
        <>
             <ProductContainer>
-                <ProductImage imageUrl={product.imageUrl}/>
+                <ProductImage imageUrl={product.imageUrl}>
+                    <CustomButton startIcon={<BsCartPlus size={18}/>}>Adicionar ao Carrinho</CustomButton>
+                </ProductImage>
 
                     <ProductInfo>
                         <p>{product.name}</p>
